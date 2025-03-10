c223a720178f704ad38a61f6e962e11588aaa576
Vince Agocs <vince.agocs@gmail.com>
2025-03-10 20:16:00 +0100

    refactor

--- src/app/helpers/getArtistList.test.ts
+++ src/app/helpers/getArtistList.test.ts
@@ -1,9 +1,33 @@
 import getArtistList from "./getArtistList";
 
+const mockUserData = { data: [{
+  "id": 690,
+  "name": "100 Folk Celsius",
+  "albumCount": 23,
+  "portrait": "https://picsum.photos/seed/690/491/544"
+},
+{
+  "id": 1035,
+  "name": "100 Tagú Cigányzenekar",
+  "albumCount": 8,
+  "portrait": "https://picsum.photos/seed/1035/477/463"
+},
+{
+  "id": 6054,
+  "name": "13 + 1 Zenekar",
+  "albumCount": 1,
+  "portrait": "https://picsum.photos/seed/6054/535/511"
+}], pagination: {
+  "current_page": 1,
+  "total_pages": 228,
+  "per_page": 50,
+  "total_items": 11382
+}};
 const mockedResponseProps = {
   headers: new Headers(),
   redirected: false,
   status: 200,
+  ok: true,
   statusText: "OK",
   type: "basic",
   url: "",
@@ -15,7 +39,7 @@ const mockedResponseProps = {
   formData: jest.fn(),
   text: jest.fn(),
   bytes: () => Promise.resolve(new Uint8Array()),
-  json: () => Promise.resolve({}),
+  json: () => Promise.resolve(mockUserData),
 }
 
 describe("Get artist list", () => {
@@ -24,36 +48,9 @@ describe("Get artist list", () => {
   })
 
   test('get artist list by page number', async () => {
-    const mockUserData = { data: [{
-        "id": 690,
-        "name": "100 Folk Celsius",
-        "albumCount": 23,
-        "portrait": "https://picsum.photos/seed/690/491/544"
-      },
-      {
-        "id": 1035,
-        "name": "100 Tagú Cigányzenekar",
-        "albumCount": 8,
-        "portrait": "https://picsum.photos/seed/1035/477/463"
-      },
-      {
-        "id": 6054,
-        "name": "13 + 1 Zenekar",
-        "albumCount": 1,
-        "portrait": "https://picsum.photos/seed/6054/535/511"
-      }], pagination: {
-        "current_page": 1,
-        "total_pages": 228,
-        "per_page": 50,
-        "total_items": 11382
-    }};
-
-
     global.fetch = jest.fn(() =>
       Promise.resolve({
-        ...mockedResponseProps,
-        ok: true,
-        json: () => Promise.resolve(mockUserData)
+        ...mockedResponseProps
       } as Response)
     );
       
@@ -62,21 +59,9 @@ describe("Get artist list", () => {
     expect(result).toEqual(mockUserData);
   });
   
-  test('throw error when fetch fails', async () => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        ...mockedResponseProps,
-        ok: false,
-        statusText: "500 Internal Server Error"
-      } as Response)
-    );
-      
-    expect(async() => await getArtistList(1)).toThrow("500 Internal Server Error")
-  });
-
   test('fetch page 1 when no page number is given', async () => {
     await getArtistList();
 
-    expect(getArtistList).toHaveBeenCalledWith(1);
+    expect(fetch).toHaveBeenCalledWith('https://exam.api.fotex.net/api/artists?include_image=true&per_page=50&page=1')
     });
 })
